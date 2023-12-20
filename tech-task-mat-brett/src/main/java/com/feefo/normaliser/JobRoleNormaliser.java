58d1042a2cf3ff55307a6ab739ed1757822059d7
Mat Brett <mat.brett@islandcity.co.uk>
2023-12-20 15:45:46 +0000

    TDD for blank validation case

--- tech-task-mat-brett/src/main/java/com/feefo/normaliser/JobRoleNormaliser.java
+++ tech-task-mat-brett/src/main/java/com/feefo/normaliser/JobRoleNormaliser.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 public class JobRoleNormaliser implements Normaliser<String> {
     @Override
     public Optional<String> normalise(final String t) {
+        if(null == t || t.isBlank()) {
+            return Optional.empty();
+        }
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
--- tech-task-mat-brett/src/test/java/com/feefo/normaliser/JobRoleNormaliserTest.java
+++ tech-task-mat-brett/src/test/java/com/feefo/normaliser/JobRoleNormaliserTest.java
@@ -1,22 +1,39 @@
 package com.feefo.normaliser;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class JobRoleNormaliserTest {
 
     private JobRoleNormaliser unitUnderTest;
 
+
+    @BeforeEach
     public void setUp() {
         unitUnderTest = new JobRoleNormaliser();
     }
 
     /**
      * Cases:
-     *
-     *  Blank
      *  No match (score is zero)
      *  Strong match (score is one)
      *  Partial but preferred match (score is between zero and one and is not 0.5)
      *  Score is split down the middle (say someone is called a Software Accountant) - this is ambigious and therefore not normalisable, this should score zero
      */
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "   ", "\t\n\r\f"})
+    public void testEmptyOptionalReturnedWhenInputIsBlank(final String unnormalisedJobRole)
+    {
+        // GIVEN a blank unnormalised input
+        // WHEN the normaliser is executed
+        // THEN no suggestion is returned
+        Assertions.assertThat(unitUnderTest.normalise(unnormalisedJobRole)).isEmpty();
+    }
 }
